4ad9e015778a0da400b1bb051ea095fbc5a57961
Danillo Xavier <102252843+danxsodev@users.noreply.github.com>
2022-10-03 15:20:32 -0300

    Add files via upload

--- calculadora/main.php
+++ calculadora/main.php
@@ -1,25 +1,31 @@
 <?php 
-include'index.html';
+include 'index.html';
 
-        $value1 = $_POST['txt-value1'];
-        $value2 = $_POST['txt-value2'];
-        $operacoes = $_POST['radio-operacoes'];
+    $value1 = $_POST['txt-value1'];
+    $value2 = $_POST['txt-value2'];
+    $operacoes = $_POST['radio-operacoes'];
+    $result;
 
-        if ($operacoes == 'radio-soma') {
-            $result = $value1 + $value2;
-        }
-        else if ($operacoes == 'radio-sub') {
-            $result = $value1 - $value2;
-        }
-        else if ($operacoes == 'radio-mult') {
-            $result = $value1 * $value2;
-        }
-        else if ($operacoes == 0) {
-            echo "<script language='javascript'>window.alert('Divisão por zero inexistente');</script>";
-        }
-        else {
-            $result = $value1 / $value2;
+    if ($operacoes == 'radio-soma') {
+        $result = $value1 + $value2;
+    }
+    else if ($operacoes == 'radio-sub') {
+        $result = $value1 - $value2;
+    }
+    else if ($operacoes == 'radio-mult') {
+        $result = $value1 * $value2;
+    }
+    else if ($value2 == 0) {
+        echo "<script language='javascript'>
+            window.alert('Divisão por zero inexistente');
+            document.getElementById('txt-result').value = 'Divisão por zero inexistente';
+        </script>";
+    }
+    else {
+        $result = $value1 / $value2;
     }
 
-    echo "O resultado é: $result";
+    echo "<script language='javascript'>
+        document.getElementById('txt-result').value = $result; 
+    </script>";
 ?>
